8ecd127b1616ad7cdcbeefff89e7a68215731f9d
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-08-11 14:03:37 +0200

    begin css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
