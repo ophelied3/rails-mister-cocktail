82f6325f397af50957291b04dcce1749984e129e
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-08-11 16:59:52 +0200

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,35 @@ list = JSON.parse(list_serialized)
 list["drinks"].each do |hash|
   Ingredient.create(name: hash["strIngredient1"])
 end
+
+Cocktail.destroy_all
+
+url = "https://static.pexels.com/photos/298719/pexels-photo-298719.jpeg"
+cocktail = Cocktail.new(name: 'Mojito')
+cocktail.save!
+cocktail.photo_url = url
+
+url = "https://static.pexels.com/photos/110472/pexels-photo-110472.jpeg"
+cocktail = Cocktail.new(name: 'Oceanique')
+cocktail.save!
+cocktail.photo_url = url
+
+url = "https://static.pexels.com/photos/109275/pexels-photo-109275.jpeg"
+cocktail = Cocktail.new(name: 'Tropical')
+cocktail.save!
+cocktail.photo_url = url
+
+url = "https://static.pexels.com/photos/5180/alcohol-party-cocktail-drink.jpg"
+cocktail = Cocktail.new(name: 'Extreme')
+cocktail.save!
+cocktail.photo_url = url
+
+url = "https://static.pexels.com/photos/274202/pexels-photo-274202.jpeg"
+cocktail = Cocktail.new(name: 'Ice')
+cocktail.save!
+cocktail.photo_url = url
+
+url = "https://static.pexels.com/photos/162915/drink-fruit-water-detox-detox-water-162915.jpeg"
+cocktail = Cocktail.new(name: 'Méditerranéen')
+cocktail.save!
+cocktail.photo_url = url
