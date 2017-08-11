4227c8b2efad4b9f5906d707ba706ac6d0df5cf2
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-08-11 11:07:55 +0200

    add attachinary

--- Gemfile
+++ Gemfile
@@ -42,7 +42,9 @@ gem 'autoprefixer-rails'
 gem 'json', '~> 1.8', '>= 1.8.3'
 
 gem 'figaro'
-gem 'cloudinary'
+gem 'cloudinary', '1.1.7'
+gem 'attachinary', github: 'assembler/attachinary'
+gem 'jquery-fileupload-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "cocktails#index"
-
+  mount Attachinary::Engine => "/attachinary"
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:create, :destroy], shallow: true
   end
